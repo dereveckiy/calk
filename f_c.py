703536dc93c1a8e8d0c859e8b7877b40a3351149
dereveckiy <parket.dreveks@gmail.com>
2025-02-07 16:48:27 +0200

    Add files via upload

---
+++ f_c.py
@@ -0,0 +1,17 @@
+from kivy.app import App
+from kivy.uix.label import Label
+
+class MainApp(App):
+    def build(self):
+        label = Label(text='Hello',
+                      size_hint=(1, 1),
+                      pos_hint={'center_x': .2, 'center_y': .8})
+        return label
+
+    
+
+
+
+if __name__ == '__main__':
+    MainApp().run()
+        	               
